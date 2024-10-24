b349ce416120f60987ab9335862333ed5c3921f6
Naveen-Joe <naveenvjv007@gmail.com>
2024-10-24 09:27:37 +0530

    Updated

--- src/Pages/Component/Landing.jsx
+++ src/Pages/Component/Landing.jsx
@@ -12,8 +12,6 @@ function Landing()
     <div>
     <div className="bg-black p-10">
     <div className="bg-[#EFEFEF] p-10 border rounded-md" >
-    <Header name={data.state.user}/> 
-    <div className="flex justify-between gap-7 my-5 flex-wrap">
     <Card bgcolor={"#8272DA"} title={"23"} subtitle={"Chennai"}/>
     <Card bgcolor={"#FD6663"} title={"December"} subtitle={"10:00:00"}/>
     <Card bgcolor={"#FCA201"} title={"Built Using"} subtitle={"React"}/>
